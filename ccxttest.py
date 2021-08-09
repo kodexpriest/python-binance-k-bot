cbfc61900d19a9e8f36c7f6fedf74814e501e7dd
kodexpriest <kodexnigrae@gmail.com>
2021-08-09 05:21:24 -0300

    2st

--- ccxttest.py
+++ ccxttest.py
@@ -1,5 +1,5 @@
 import ccxt
-import yfinance as yf
+
 import pandas as pd
 import pandas_ta as ta
 
@@ -8,5 +8,8 @@ exchange = ccxt.binance()
 bars = exchange.fetch_ohlcv('BNBUSDT', timeframe='5m', limit=500)
 
 df = pd.DataFrame(bars, columns=['time', 'open', 'high', 'low', 'close', 'volume'])
+atr = df.ta.atr(length=14)
+df = pd.concat([df, atr], axis=1)
+
 
 print(df)
--- main.py
+++ main.py
@@ -1,30 +1,54 @@
 #imports
-import datetime, time, pprint
-
-from matplotlib.pyplot import tight_layout
+import datetime, time
+from pprint import pprint as pp
 import config #binance api key and secret
 import pandas as pd
 import pandas_ta as ta
-import yfinance as yf
+
 from binance.client import Client
 from binance.enums import KLINE_INTERVAL_1MINUTE
 
 #const
-SYMBOL = 'BNBUSDT'
+SYMBOL = 'BTCUSDT'
 client = Client(config.API_KEY, config.API_SECRET)
 
+
+#functions
 def get_candles(symbol:str = SYMBOL, limit:int = 500):
     full_candles = pd.DataFrame(data=client.get_klines(
-        symbol=SYMBOL,
+        symbol=symbol,
         interval=KLINE_INTERVAL_1MINUTE,
         limit=limit),
         columns= ['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Close_time', 'Quote', 'Trades', 'TBBAV', 'TBQAV', 'Ignore'])
     candles = full_candles.loc[:limit,['Date', 'Open', 'High', 'Low', 'Close', 'Volume']]                            # menos columnas
-    candles = candles.astype({'Open': float, 'High': float, 'Low': float, 'Close': float, 'Volume': float})     # de object a float64
-    candles['Date'] = pd.to_datetime(candles['Date'])                                                           # de int a datetime
+    candles = candles.astype({'Open': float, 'High': float, 'Low': float, 'Close': float, 'Volume': float})         # de object a float64
+    candles['Date'] = pd.to_datetime(candles['Date'])                                                               # de int a datetime
     candles.set_index('Date', inplace=True)
-    return candles
+    
+    atr = candles.ta.atr(length=5)
+    result = pd.concat([candles, atr], axis=1)
+    return result
 
-candles = get_candles(limit=500)
+def candles(limit:int = 10):
+    candles = get_candles(limit=limit)
+    return candles.to_html()
 
-print(candles)
+def get_balance():
+    balance = {}
+    b = client.futures_account_balance()
+    for k,v in enumerate(b):
+        balance.update({k:{'asset':v['asset'],'balance':v['balance']}})
+    return balance
+
+def get_info():
+    info = client.get_exchange_info()
+    return info
+
+def get_orders():
+    orders = client.futures_get_open_orders()
+    return orders
+
+#app0
+if __name__ == '__main__':
+    print(get_candles(symbol='ETHUSDT',limit=1))
+    print(get_balance())
